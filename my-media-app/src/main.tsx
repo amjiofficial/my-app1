f2b374482758d847493fbe467b4fce2447793bf1
amjiofficial <amjadkhanofficial124@gmail.com>
2026-02-13 12:12:23 +0500

    Fix: Use Amplify.configure with explicit Storage config for bucket name

--- my-media-app/src/main.tsx
+++ my-media-app/src/main.tsx
@@ -1,22 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Amplify } from 'aws-amplify'
-import { configure } from '@aws-amplify/storage'
 import awsExports from './aws-exports'
 import App from './App'
 import './index.css'
 
-Amplify.configure(awsExports)
-
-// Explicitly configure Storage for Amplify v6
-if (awsExports.aws_storage_s3_bucket) {
-  configure({
+// Configure Amplify with Storage settings explicitly included
+Amplify.configure({
+  ...awsExports,
+  Storage: {
     S3: {
       bucket: awsExports.aws_storage_s3_bucket,
       region: awsExports.aws_storage_s3_region || awsExports.aws_project_region,
     },
-  })
-}
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
