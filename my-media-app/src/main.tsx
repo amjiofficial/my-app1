a3407ea846350ebeadaa8287648b7ec7c434bfb0
amjiofficial <amjadkhanofficial124@gmail.com>
2026-02-13 12:11:59 +0500

    Fix: Configure Storage explicitly for Amplify v6 to resolve bucket name error

--- my-media-app/src/main.tsx
+++ my-media-app/src/main.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Amplify } from 'aws-amplify'
+import { configure } from '@aws-amplify/storage'
 import awsExports from './aws-exports'
 import App from './App'
 import './index.css'
 
 Amplify.configure(awsExports)
+
+// Explicitly configure Storage for Amplify v6
+if (awsExports.aws_storage_s3_bucket) {
+  configure({
+    S3: {
+      bucket: awsExports.aws_storage_s3_bucket,
+      region: awsExports.aws_storage_s3_region || awsExports.aws_project_region,
+    },
+  })
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
